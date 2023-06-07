e60f68c1ea2e47d7fefc9f1c219b047a67b875f5
Mateus Cazuza <mateus.cazuza@hotmail.com>
2023-06-07 14:41:56 -0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-function App() {
+import { Post } from './Post';
+import { Header } from './components/Header';
+import './styles.css';
 
+export function App() {
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <h1>Feedback</h1>
+      <Header />
+      <Post
+        author='Mateus Cazuza'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illum eos nulla quidem fugit enim accusantium amet optio totam repellat, eius impedit saepe accusamus architecto dicta in qui ipsam odit.'
+      />
+      <Post
+        author='Isaac'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illum eos nulla quidem fugit enim accusantium amet optio totam repellat, eius impedit saepe accusamus architecto dicta in qui ipsam odit.'
+      />
+      <Post
+        author='Raimundo Cazuza'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illum eos nulla quidem fugit enim accusantium amet optio totam repellat, eius impedit saepe accusamus architecto dicta in qui ipsam odit.'
+      />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
 }
-
-export default App
